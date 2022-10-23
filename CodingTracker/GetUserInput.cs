0437d0155c1e595da046d5a1ae9cd630bc2992b6
Nonso <Onwulinonso@gmail.com>
2022-10-23 02:47:37 +0100

    Add project files.

---
+++ CodingTracker/GetUserInput.cs
@@ -0,0 +1,16 @@
+﻿internal class GetUserInput
+{
+    internal void MainMenu()
+    {
+        bool closeApp = false;
+        while (closeApp == false)
+        {
+            Console.WriteLine("\n\nMAIN MENU");
+            Console.WriteLine("\nWhat woud you like to do");
+            Console.WriteLine("Type 1 to view record");
+            Console.WriteLine("Type 2 to Add record");
+            Console.WriteLine("Type 3 to Delete record");
+            Console.WriteLine("Type 4 to Update record");
+        }
+    }
+}
---
+++ CodingTracker/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Configuration;
+
+class Program
+{
+    static string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+
+    private static void Main(string[] args)
+    {
+        DatabaseManager databaseManager = new();
+        GetUserInput getUserInput = new();
+
+        databaseManager.CreateTable(connectionString);
+
+        getUserInput.MainMenu():
+    }
+}
